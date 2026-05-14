f0cf78985f99f4f9fe844ca7ba7bda9517bb2db9
Raul <72282208+RaulPellegrini@users.noreply.github.com>
2026-05-14 12:13:02 +0200

    inheritances

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,7 +4,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField] float health = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void TakeDamage(float damage)
+    public virtual void TakeDamage(float damage)
     {
         health -= damage;
         //Debug.Log($"DAMAGER TAKEN! {health}");
